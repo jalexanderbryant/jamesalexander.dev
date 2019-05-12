9c8e902f904593a6a73b4abb38e127badcd0641d
James Alexander <j.alexanderbryant@gmail.com>
2019-05-12 01:24:40 -0400

    Initial Pelican setup.

---
+++ pelicanconf.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'James Alexander'
+SITENAME = 'James Alexander'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'America/New_York'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('LinkedIn', 'https://www.linkedin.com/in/james-alexander-946a0689'),
+          ('Github', 'https://github.com/jalexanderbryant'),
+          ('Twitter', 'https://twitter.com/cynicalneon'),
+          ('Instagram', 'https://www.instagram.com/jalexanderbryant/'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
